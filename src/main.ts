e775a9d210f9b3644fdf6ccc36a16abb934b4e28
DexZed <144045080+DexZed@users.noreply.github.com>
2026-09-15 21:00:29 +0600

    bug fixes

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,14 @@ import { CatchEverythingFilter } from './common/exceptions/global.exceptions.js'
 import { TransformInterceptor } from './lib/transform.interceptor.js';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+  app.setGlobalPrefix('api');
+
   const httpAdapter = app.get(HttpAdapterHost);
   app.useGlobalFilters(new CatchEverythingFilter(httpAdapter));
   app.useGlobalInterceptors(new TransformInterceptor());
+  await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
