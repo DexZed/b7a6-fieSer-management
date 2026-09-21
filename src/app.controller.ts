93c68bf4dcf82ac79615542b73a22f21d5609964
DexZed <144045080+DexZed@users.noreply.github.com>
2026-09-21 21:43:32 +0600

    small fixes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service.js';
 import { AllowAnonymous } from '@thallesp/nestjs-better-auth';
-import type { Response as ExpressResponse } from 'express';
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
