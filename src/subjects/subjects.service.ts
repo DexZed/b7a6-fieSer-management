7e8c718e23ae15a4baec32eb0d463ed1ec9497be
DexZed <144045080+DexZed@users.noreply.github.com>
2026-09-23 12:51:19 +0600

    bug fix subjects

--- src/subjects/subjects.service.ts
+++ src/subjects/subjects.service.ts
@@ -184,7 +184,7 @@ export class SubjectsService {
       if (role === 'teacher') {
         const where = {
           role: 'teacher',
-          classesTaught: {
+          classes: {
             some: { subjectId },
           },
         };
