1e29686ea5705cc9f142ef7c0f6d084ec7c9cd19
DexZed <144045080+DexZed@users.noreply.github.com>
2026-09-23 17:47:11 +0600

    csr fix

---
+++ public/google.js
@@ -0,0 +1,20 @@
+document.getElementById('google').onclick = async () => {
+        const response = await fetch('/api/auth/sign-in/social', {
+          method: 'POST',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            provider: 'google',
+          }),
+        });
+
+        const data = await response.json();
+
+        console.log(data);
+
+        if (data.url) {
+          window.location.href = data.url;
+        }
+      };
